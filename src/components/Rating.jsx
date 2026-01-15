c3d1225a605678d4253c4079213e88bea921795d
dev-abubakarsharif <abubakarsharif.dev@gmail.com>
2026-01-15 17:56:38 +0100

    added a submit button

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Star from "./Star";
-const Rating = ({ heading = "Rate Your Experience", color = 'gold' }) => {
+const Rating = ({ heading = "Rate Your Experience", color = "gold" }) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    rating > 0 && setSubmitted(true)
+  };
   //  setRating()
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
@@ -13,29 +16,38 @@ const Rating = ({ heading = "Rate Your Experience", color = 'gold' }) => {
       <h2>{heading}</h2>
       <div className="stars">
         {stars.map((star, index) => (
-          <Star 
-          key={star} 
-          star={star}
-          rating ={rating}
-          hover = {hover}
-          color = {color}
-          ratingClick = {setRating}
-          setHover={setHover}
+          <Star
+            key={star}
+            star={star}
+            rating={rating}
+            hover={hover}
+            color={color}
+            ratingClick={setRating}
+            setHover={setHover}
           />
-          // <span
-          //   onClick={() => setRating(star)}
-          //   onMouseEnter={() => setHover(star)}
-          //   onMouseLeave={() => setHover(0)}
-          //   key={star}
-          //   className={`star ${star <= (hover || rating) ? "active" : ""}`}
-          // >
-          //   {"\u2605"}
-          // </span>
         ))}
       </div>
       {rating > 0 && (
         <p className="feedback"> {feedbackMessages[rating - 1]}</p>
       )}
+      <button
+        className="submit-btn"
+        onClick={() => handleSubmit()}
+        disabled={rating === 0}
+      >
+        Submit
+      </button>
+      {/* modal */}
+      {
+        submitted && (
+          <div className="modal-overlay">
+            <div className="modal">
+              <h2>Thank You!</h2>
+              <p>You rated us {rating} star{rating > 1 && 's' }</p>
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 };
